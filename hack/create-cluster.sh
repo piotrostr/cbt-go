7782374ebb8d9d3afe45cdca6115f79ce52488ef
Piotr Ostrowski <piotrostr@google.com>
2022-09-14 17:47:37 +0100

    nit: remove &&

--- hack/create-cluster.sh
+++ hack/create-cluster.sh
@@ -3,7 +3,7 @@
 cluster_id=cbt-filler-cluster
 
 gcloud container clusters create-auto $cluster_id \
-    --region us-central1 && \
-\
+    --region us-central1
+
 gcloud container clusters get-credentials $cluster_id \
     --region us-central1
