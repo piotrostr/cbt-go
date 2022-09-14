e671bed73afa3eb776b1cf2a0a7d62bbc141d330
Piotr Ostrowski <piotrostr@google.com>
2022-09-14 17:40:37 +0100

    add cleanup script

---
+++ hack/clean-up.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+instance_id=my-instance-id
+cluster_id=cbt-filler-cluster
+
+gcloud bigtable instances delete $instance_id \
+        --quiet \
+        --async
+
+gcloud container clusters delete $cluster_id \
+        --region us-central1 \
+        --quiet \
+        --async
