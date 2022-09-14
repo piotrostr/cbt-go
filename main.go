ccc04495f4bff31b9e2dc9d423b35a373821c00d
Piotr Ostrowski <piotrostr@google.com>
2022-09-14 21:48:15 +0100

    add flag steering

--- main.go
+++ main.go
@@ -2,22 +2,49 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"time"
 
 	"github.com/piotrostr/cbt-go/bt"
 )
 
+var (
+	projectID = flag.String(
+		"project",
+		"piotrostr-resources",
+		"The Google Cloud project ID",
+	)
+	instanceID = flag.String(
+		"instance",
+		"my-instance-id",
+		"The Cloud Bigtable instance ID",
+	)
+	tableName = flag.String(
+		"table",
+		"mobile-time-series",
+		"The Cloud Bigtable table ID",
+	)
+	columnFamilyName = flag.String(
+		"column-family",
+		"stats_summary",
+		"The Cloud Bigtable column family ID",
+	)
+	runWrite = flag.Bool(
+		"write",
+		false,
+		"Run write example",
+	)
+	runRead = flag.Bool(
+		"read",
+		false,
+		"Run read example",
+	)
+)
+
 var ctx = context.Background()
 
-func main() {
-	cfg := &bt.Config{
-		ProjectID:        "piotrostr-resources",
-		InstanceID:       "my-instance-id",
-		TableName:        "mobile-time-series",
-		ColumnFamilyName: "stats_summary",
-	}
-
+func Write(ctx context.Context, cfg *bt.Config) {
 	if err := bt.CreateTableIfNotExists(ctx, cfg); err != nil {
 		fmt.Printf("createTableIfNotExists failed: %v\n", err)
 		return
@@ -42,5 +69,25 @@ func main() {
 			fmt.Printf("write successful in %d ms\n", elapsed)
 			<-devices
 		}()
+	}
+
+}
+
+func main() {
+	flag.Parse()
+
+	cfg := &bt.Config{
+		ProjectID:        *projectID,
+		InstanceID:       *instanceID,
+		TableName:        *tableName,
+		ColumnFamilyName: *columnFamilyName,
+	}
+
+	if *runWrite {
+		Write(ctx, cfg)
+	} else if *runRead {
+		fmt.Println("read")
+	} else {
+		fmt.Println("nothing to do, use --write or --read flag")
 	}
 }
