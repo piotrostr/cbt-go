8ace74c3a09801a7e58200e5d41d38ff1714fad0
Piotr Ostrowski <piotrostr@google.com>
2022-09-14 17:56:06 +0100

    feat: enable vpa

--- hack/create-cluster.sh
+++ hack/create-cluster.sh
@@ -3,7 +3,8 @@
 cluster_id=cbt-filler-cluster
 
 gcloud container clusters create-auto $cluster_id \
-    --region us-central1
+    --region us-central1 \
+    --enable-vertical-pod-autoscaling
 
 gcloud container clusters get-credentials $cluster_id \
     --region us-central1
