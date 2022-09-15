e8f57abac70651323a431d8127b4727bdfdad460
Piotr Ostrowski <piotrostr@google.com>
2022-09-15 21:04:41 +0100

    fix cleanup script

--- hack/clean-up.sh
+++ hack/clean-up.sh
@@ -4,11 +4,10 @@ instance_id=iot-example-instance
 cluster_id=cpu-intensive-cluster
 region=us-central1
 
-gcloud bigtable instances delete $instance_id \
-    --quiet \
-    --async
-
 gcloud container clusters delete $cluster_id \
     --region $region \
     --quiet \
     --async
+
+gcloud bigtable instances delete $instance_id \
+    --quiet 
