a81184a1f23a4db16c302077ed8447f9e488b352
Piotr Ostrowski <piotrostr@google.com>
2022-09-16 01:28:57 +0100

    add node pool oauth scopes

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -15,7 +15,7 @@ resource "google_service_account" "gcr_sa" {
 
 resource "google_project_iam_binding" "storage_admin" {
   project = var.project
-  role    = "roles/storage.objectViewer"
+  role    = "roles/storage.admin"
   members = [
     "serviceAccount:${google_service_account.gcr_sa.email}"
   ]
@@ -35,5 +35,9 @@ resource "google_container_node_pool" "cpu_intensive" {
     service_account = google_service_account.gcr_sa.email
     spot            = true
     machine_type    = "c2d-highcpu-8"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+    ]
   }
 }
